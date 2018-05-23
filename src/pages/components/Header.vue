5ee1e42ab71b5fdbd9cc79cfacb2100a9ca6fcb9
hw58672960 <2012497408@qq.com>
2018-05-23 11:52:10 +0800

    some first preparation

---
+++ src/pages/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="header-left"> 返回</div>
+    <div class="header-input">内容</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background:#00bcd4
+    color :#fff
+  .header-left
+    width: .64rem
+    float: left
+.header_input
+  flex:1;
+  .header-right
+    width: 1.24rem
+    float: right
+
+</style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,17 @@
+<!--suppress ALL -->
+<template>
+    <home-header>This is header</home-header>
+</template>
+
+<script>
+import Homeheader from "./components/Header"
+export default s{
+    name: "Header",
+    components:{
+        Homeheader:Homeheader,
+    }
+}
+</script>
+
+<style>
+</style>
